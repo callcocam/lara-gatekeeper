af550a055cfecfceb4e1f3ea9ec3921fe15549f4
callcocam <callcocam@gmail.com>
2025-04-08 09:28:01 -0300

    feat: Add migration files and setup command for LaraGatekeeper package

--- src/LaraGatekeeperServiceProvider.php
+++ src/LaraGatekeeperServiceProvider.php
@@ -1,14 +1,18 @@
 <?php
+
 /**
  * Created by Claudio Campos.
  * User: callcocam@gmail.com, contato@sigasmart.com.br
  * https://www.sigasmart.com.br
  */
+
 namespace Callcocam\LaraGatekeeper;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 use Callcocam\LaraGatekeeper\Commands\LaraGatekeeperCommand;
+use Callcocam\LaraGatekeeper\Commands\LaraGatekeeperSetupCommand;
+use Spatie\LaravelPackageTools\Commands\InstallCommand;
 
 class LaraGatekeeperServiceProvider extends PackageServiceProvider
 {
@@ -23,7 +27,32 @@ class LaraGatekeeperServiceProvider extends PackageServiceProvider
             ->name('lara-gatekeeper')
             ->hasConfigFile()
             ->hasViews()
-            ->hasMigration('create_lara_gatekeeper_table')
-            ->hasCommand(LaraGatekeeperCommand::class);
+            ->hasMigrations(
+                'alter_users_table',
+                'create_tenants_table',
+                'create_addresses_table',
+                'create_roles_table',
+                'create_permissions_table',
+                'create_permission_role_table',
+                'create_role_user_table',
+                'create_permission_user_table'
+            )
+            ->hasCommand(LaraGatekeeperCommand::class)
+            ->hasTranslations()
+            ->hasAssets()
+            ->hasCommand(LaraGatekeeperSetupCommand::class)
+            ->hasInstallCommand(function (InstallCommand $command) {
+                $command
+                    ->publishConfigFile()
+                    ->publishAssets()
+                    ->publishMigrations()
+                    ->publish('lara-gatekeeper:translations')
+                    ->askToRunMigrations()
+                    ->copyAndRegisterServiceProviderInApp()
+                    ->askToStarRepoOnGitHub('callcocam/lara-gatekeeper')
+                    ->endWith(function (InstallCommand $command) {
+                        $command->call('lara-gatekeeper:setup');
+                    });
+            });
     }
 }
